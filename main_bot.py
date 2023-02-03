0ba627aa5c2b564cde5c09c2eaf0c8f2a8c737bb
JAndresMenchaca <joseandres190503@gmail.com>
2023-02-03 12:57:11 -0400

    Add voice channel

--- main_bot.py
+++ main_bot.py
@@ -1,14 +1,15 @@
 import json, os
 import discord
 from discord.ext import commands
-TOKEN = "MTA3MDg1NDk3MjQ5MDY2NjA0Ng.Gs0YD0.YvBUXGwiusqjcuiXk0y7J8FylwGetFiRq2L-aY"
+from discord.utils import get
+TOKEN = ""
 
 def main():
 	if os.path.exists("config.json"):
 		with open('config.json') as f:
 			config_data = json.load(f)
 	else:
-		template = {'prefix':'!', 'token':TOKEN}
+		template = {'prefix':'mp!', 'token':TOKEN}
 		with open('config.json','w') as f:
 			json.dump(template, f)
 
@@ -20,6 +21,40 @@ def main():
 	@bot.command(name="saludar")
 	async def saludar(ctx):
 		await ctx.reply("Hola, como estas??")
+
+	@bot.command(name="suma")
+	async def suma(ctx, arg1, arg2):
+		suma = int(arg1) + int(arg2)
+		await ctx.send(suma)
+
+	@bot.command(name="helpme")
+	async def helpme(ctx):
+		embed = discord.Embed(title="Bot de Ayuda", description="Aqui estan los comandos")
+		embed.add_field(name="mp!ping", value="Responde pong")
+		await ctx.send(content=None, embed = embed)
+
+	@bot.command(name="conect", pass_context = True)
+	async def come(ctx):
+
+		try:
+			channel = ctx.message.author.voice.channel
+
+			voice = get(bot.voice_clients, guild=ctx.guild)
+
+			if voice and voice.is_connected():
+				await voice.move_to(channel)
+			else:
+				voice = await channel.connect()
+		except:
+			await ctx.send("No estas conectado a un canal de voz")
+
+	@bot.command(pass_context = True)
+	async def disconect(ctx):
+		channel = ctx.message.author.voice.channel
+		voice = get(bot.voice_clients, guild = ctx.guild)
+		await voice.disconnect()
+
+
 
 
 	@bot.event
@@ -37,6 +72,12 @@ def main():
 			await message.channel.send("Pong")
 
 		await bot.process_commands(message)
+
+	@bot.event
+	async def on_ready():
+		await bot.change_presence(activity= discord.Game(name="jugando en el servidor"))
+
+
 
 	bot.run(token)
 
